18558337887cd5839636c11dffac2a0742fe2f76
Javier Chacana <javierchacana@gmail.com>
2020-03-24 00:17:29 +0100

    [Javier] Immutability principle applied

--- src/main/java/com/natalita/StringCalculator.java
+++ src/main/java/com/natalita/StringCalculator.java
@@ -9,27 +9,27 @@ public class StringCalculator {
     public static final String NEWLINE = "\n";
     public static final String PREFIX = "//";
 
-    public int add(String input) {
+    public int add(final String input) {
         if(input.isEmpty())
             return 0;
-
+        String numbers = input;
         String separator = COMA_OR_NEWLINE;
         if(input.startsWith(PREFIX)) {
-            separator = identifySeparator(input);
-            input = getNumbers(input);
+            separator = identifySeparator(numbers);
+            numbers = getNumbers(numbers);
         }
-        return getSumFromStringNumbers(input.split(separator));
+        return getSumFromStringNumbers(numbers.split(separator));
     }
 
-    private String getNumbers(String input) {
+    private String getNumbers(final String input) {
         return input.split(NEWLINE)[1];
     }
 
-    private String identifySeparator(String numbers) {
+    private String identifySeparator(final String numbers) {
         return numbers.substring(PREFIX.length(), numbers.indexOf(NEWLINE));
     }
 
-    private int getSumFromStringNumbers(String[] numbers) {
+    private int getSumFromStringNumbers(final String[] numbers) {
         return Arrays.stream(numbers)
                 .mapToInt(value -> Integer.parseInt(value))
                 .sum();
